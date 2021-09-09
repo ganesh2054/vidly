5418b78588ed4f0e77c38015af3fb23fc42a0f03
Ganesh <lmeganesh123@gmail.com>
2021-09-09 15:23:46 +0545

    updating sort

---
+++ src/Components/tableHeader.jsx
@@ -0,0 +1,9 @@
+ import React, { PureComponent } from 'react';
+
+ class TableHeader extends React.Component {
+     render() { 
+         return <div></div>;
+     }
+ }
+  
+ export default TableHeader;
