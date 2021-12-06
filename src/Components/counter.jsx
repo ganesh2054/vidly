cc7e4fe56eb2bdda7eb364b34ef76e5c1089b09b
Ganesh <ganeshlamichhane202@gmail.com>
2021-12-06 10:33:39 +0545

    updating

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -6,7 +6,7 @@ class Counter extends React.Component {
             <span>{this.props.counter.value}</span>
             <button className="mt-2 ml-5" onClick={()=>this.props.onIncrement(this.props.counter)}>Increment</button>
             {/* <button onClick={()=>this.props.onDelete(this.props.id)} >Delete</button> */}
-            <button className="mt-2 ml-5" onClick={()=>this.props.onDelete(this.props.counter.id)} >Delete</button>
+            <button className="mt-2 ml-5" onClick={()=>this.props.onDelete(this.props.counter.id)} >Remove</button>
         
         </>;
     }
